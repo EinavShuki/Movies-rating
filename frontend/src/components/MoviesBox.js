0a932cac2b9e4725d896191ff02041e7b8078608
Einav1 <einavshpigel@gmail.com>
2021-02-28 19:03:13 +0200

    make css better

--- frontend/src/components/MoviesBox.js
+++ frontend/src/components/MoviesBox.js
@@ -5,25 +5,23 @@ const MoviesBox = ({ movies }) => {
   return (
     <div className="movies_grid">
       {movies.map((movie, index) => (
-        <div className="movies zoomIn" key={index}>
-          <Link className="arl" to={`/movie/${movie.imdbID}`}>
-            To all reviews
-          </Link>
-          <div className="movie-info">
-            <img
-              alt={movie.Title}
-              src={
-                movie.Poster === "N/A"
-                  ? "https://placehold.it/198x264&text=Image+Not+Found"
-                  : movie.Poster
-              }
-            />
-
-            <p style={{ marginBottom: "1rem" }}>
-              {movie.Title} | {movie.Year}
-            </p>
+        <Link key={index} className="arl" to={`/movie/${movie.imdbID}`}>
+          <div className="movies zoomIn">
+            <div className="movie-info">
+              <img
+                alt={movie.Title}
+                src={
+                  movie.Poster === "N/A"
+                    ? "https://placehold.it/198x264&text=Image+Not+Found"
+                    : movie.Poster
+                }
+              />
+              <p style={{ marginBottom: "1rem" }}>
+                {movie.Title} | {movie.Year}
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
