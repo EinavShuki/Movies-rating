9f8a1b2da05f613515de728f2065aba342c18978
Einav1 <einavshpigel@gmail.com>
2021-02-28 13:01:17 +0200

    try connect web

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import moviesRoutes from "./router/moviesRoutes.js";
 import connectDB from "./config/db.js";
+import path from "path";
 
 const app = express();
 
@@ -14,6 +15,20 @@ app.use(express.json());
 console.log("in server");
 
 app.use("/api/movies", moviesRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
